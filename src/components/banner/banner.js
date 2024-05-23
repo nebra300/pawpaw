1705807318a3ca47fdc090b16959449e2d10119e
bmorovic <branimir.morovic_ext@autoscout24.com>
2024-05-23 17:04:53 +0200

    adding puppy comment

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,14 +1,42 @@
 import './banner.css'
-import dog245 from '../../images/elipse1.jpg'
-import dog989 from '../../images/elipse2.png'
-import dog98 from '../../images/elipse3.jpg'
-import dog67 from '../../images/tlo.jpg'
 import PuppyComment from '../puppy-comment/Puppycomment'
+import lifegoodpuppy from '../../images/elipse1.jpg'
 
 function Banner() {
     return(
-    <div className='banner-wrapper'>
-        <PuppyComment />
+    <div className='big-wrapper'>
+        <div className='banner-wrapper'>
+            <div className='banner-section-column'>
+                <PuppyComment 
+                    title={"lives good"}
+                    paragraph={"Lots of food, lots of walkses and lots of food. After i finish paw school I might get good job to repay my hoomans. Then I give them treats... Until then i just cute"}
+                    name={"Jack"}
+                    image={lifegoodpuppy}
+                    isReverseRow={false}
+                />
+
+                <PuppyComment 
+                    title={"lives not good"}
+                    paragraph={"Lots of food, lots of walkses and lots of food. After i finish paw school I might get good job to repay my hoomans. Then I give them treats... Until then i just cute"}
+                    name={"Jack"}
+                    image={lifegoodpuppy}
+                    isReverseRow={true}
+                />
+            </div>
+            <div className='banner-section-title'>
+                <h1>Fluffs That Found a Home Through PawPaw</h1>
+            </div>
+            <div className='banner-section-row'>
+                <PuppyComment 
+                    title={"lives not good"}
+                    paragraph={"Lots of food, lots of walkses and lots of food. After i finish paw school I might get good job to repay my hoomans. Then I give them treats... Until then i just cute"}
+                    name={"Jack"}
+                    image={lifegoodpuppy}
+                    isReverseColumn={true}
+                />
+                <span className='happy-customers'>Over 300 happy customers...and their hoomans</span>
+            </div>
+        </div>
     </div>
     )
 }
--- src/components/puppy-comment/Puppycomment.js
+++ src/components/puppy-comment/Puppycomment.js
@@ -1,24 +1,42 @@
 import './Puppycomment.css'
-import lifegoodpuppy from '../../images/elipse1.jpg'
+
+function PuppyComment({
+    title,
+    paragraph,
+    name,
+    image,
+    isReverseRow,
+    isReverseColumn, 
+}){
+    /*
+        x===something   true/false
+        x!==something   true/false
+        x>something     true/false
+        x>=something    true/false
+        x<something     true/false
+        x<=something    true/false
+    */
+
+    var direction;  //undefined
+
+    if(isReverseRow !== undefined) {
+        direction = isReverseRow ? 'row-reverse' : 'row';
+    }
+
+    if(isReverseColumn !== undefined) {
+        direction = isReverseColumn ? 'column-reverse' : 'column';
+    }
 
 
-function PuppyComment(){
     return(
-        <div className='puppy-comment-wrapper'>
-    <div className='puppy-comment-text-wrapper'>
-    <span className='puppy-comment-title'>lives good</span>
-    <span className='puppy-comment-paragraph'>
-Lots of food, lots of walkses and lots of food. After i finish paw school I might get good job to repay my hoomans.
-Then I give them treats...
-Until then i just cute
-    </span>
-    <span className='puppy-comment-name'>Jack</span>
+    <div className='puppy-comment-wrapper' style={{flexDirection: direction}}>
+        <div className='puppy-comment-text-wrapper'>
+        <span className='puppy-comment-title'>{title}</span>
+        <span className='puppy-comment-paragraph'>{paragraph}</span>
+        <span className='puppy-comment-name'>{name}</span>
+        </div>
+        <img className='puppy-comment-images' src={image}/>
     </div>
-    <img className='puppy-comment-images' src={lifegoodpuppy}/>
-    </div>
-        
-
-
-)
+    )
 }
 export default PuppyComment
